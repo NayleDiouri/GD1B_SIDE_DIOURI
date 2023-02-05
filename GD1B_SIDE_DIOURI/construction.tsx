f803476518aa9e2f76a72443678b8a0d4a9f4e97
NayleDiouri <93705247+NayleDiouri@users.noreply.github.com>
2023-02-05 16:59:48 +0100

    Add files via upload

---
+++ GD1B_SIDE_DIOURI/construction.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="construction" tilewidth="1600" tileheight="1600" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="1600" height="1600" source="ld niveau.png"/>
+ </tile>
+</tileset>
